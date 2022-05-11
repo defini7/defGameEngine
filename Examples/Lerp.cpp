8ee68b084e277f6d96d2d5d8d7963cc493db3d76
Alex <75216201+defini7@users.noreply.github.com>
2022-05-11 15:55:04 +0300

    Update Lerp.cpp

--- Examples/Lerp.cpp
+++ Examples/Lerp.cpp
@@ -19,21 +19,21 @@ protected:
 		float t = 0.0f;
 		for (int x = 0; x < 84; x += 8)
 		{
-			FillRect(x + 20, 20 + 8 * 0, 8, 8, def::BLUE * t);
-			FillRect(x + 20, 20 + 8 * 1, 8, 8, def::CYAN * t);
-			FillRect(x + 20, 20 + 8 * 2, 8, 8, def::DARK_BLUE * t);
-			FillRect(x + 20, 20 + 8 * 3, 8, 8, def::DARK_CYAN * t);
-			FillRect(x + 20, 20 + 8 * 4, 8, 8, def::DARK_GREEN * t);
-			FillRect(x + 20, 20 + 8 * 5, 8, 8, def::DARK_GREY * t);
-			FillRect(x + 20, 20 + 8 * 6, 8, 8, def::DARK_MAGENTA * t);
-			FillRect(x + 20, 20 + 8 * 7, 8, 8, def::DARK_RED * t);
-			FillRect(x + 20, 20 + 8 * 8, 8, 8, def::ORANGE * t);
-			FillRect(x + 20, 20 + 8 * 9, 8, 8, def::GREEN * t);
-			FillRect(x + 20, 20 + 8 * 10, 8, 8, def::GREY * t);
-			FillRect(x + 20, 20 + 8 * 11, 8, 8, def::MAGENTA * t);
-			FillRect(x + 20, 20 + 8 * 12, 8, 8, def::RED * t);
-			FillRect(x + 20, 20 + 8 * 13, 8, 8, def::WHITE * t);
-			FillRect(x + 20, 20 + 8 * 14, 8, 8, def::YELLOW * t);
+			FillRectangle(x + 20, 20 + 8 * 0, 8, 8, def::BLUE * t);
+			FillRectangle(x + 20, 20 + 8 * 1, 8, 8, def::CYAN * t);
+			FillRectangle(x + 20, 20 + 8 * 2, 8, 8, def::DARK_BLUE * t);
+			FillRectangle(x + 20, 20 + 8 * 3, 8, 8, def::DARK_CYAN * t);
+			FillRectangle(x + 20, 20 + 8 * 4, 8, 8, def::DARK_GREEN * t);
+			FillRectangle(x + 20, 20 + 8 * 5, 8, 8, def::DARK_GREY * t);
+			FillRectangle(x + 20, 20 + 8 * 6, 8, 8, def::DARK_MAGENTA * t);
+			FillRectangle(x + 20, 20 + 8 * 7, 8, 8, def::DARK_RED * t);
+			FillRectangle(x + 20, 20 + 8 * 8, 8, 8, def::ORANGE * t);
+			FillRectangle(x + 20, 20 + 8 * 9, 8, 8, def::GREEN * t);
+			FillRectangle(x + 20, 20 + 8 * 10, 8, 8, def::GREY * t);
+			FillRectangle(x + 20, 20 + 8 * 11, 8, 8, def::MAGENTA * t);
+			FillRectangle(x + 20, 20 + 8 * 12, 8, 8, def::RED * t);
+			FillRectangle(x + 20, 20 + 8 * 13, 8, 8, def::WHITE * t);
+			FillRectangle(x + 20, 20 + 8 * 14, 8, 8, def::YELLOW * t);
 			t += 0.1f;
 		}
 
