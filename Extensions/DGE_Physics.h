307dab9f621cb3ab6a0291bb000b474e2760e69d
Alex <75216201+defini7@users.noreply.github.com>
2022-06-16 13:42:48 +0300

    Update DGE_Physics.h

--- Extensions/DGE_Physics.h
+++ Extensions/DGE_Physics.h
@@ -31,6 +31,7 @@
 #pragma region includes
 
 #include "defGameEngine.h"
+
 #include <algorithm>
 
 #pragma endregion
@@ -39,15 +40,17 @@ namespace def
 {
 	struct sShape
 	{
-		def::vf2d vStart;
-		def::vf2d vVelocity;
+		vf2d vVelocity;
+		vf2d vStart;
 	};
 
 	struct sPoint : sShape {};
 
 	struct sRectangle : sShape
 	{
-		def::vf2d vEnd;
+		vf2d vEnd;
+
+		sRectangle* contact[4];
 	};
 
 	struct sCircle : sShape
@@ -57,13 +60,12 @@ namespace def
 
 	struct sLine : sShape
 	{
-		def::vf2d vEnd;
+		vf2d vEnd;
 	};
 
 	class DGE_Physics
 	{
 	public:
-
 		bool PointVsCircle(const sPoint& p, const sCircle& c)
 		{
 			return (c.vStart - p.vStart).mag2() <= c.fRadius * c.fRadius;
@@ -79,15 +81,15 @@ namespace def
 			return r1.vStart - 1 < r2.vEnd && r1.vEnd + 1 > r2.vStart;
 		}
 
-		bool LineVsRect(const sLine& l, const sRectangle& r, def::vf2d& contact_point, def::vf2d& contact_normal, float& t_hit_near)
+		bool LineVsRect(const sLine& l, const sRectangle& r, vf2d& contact_point, vf2d& contact_normal, float& t_hit_near)
 		{
 			contact_normal = { 0,0 };
 			contact_point = { 0,0 };
 
-			def::vf2d invdir = def::vf2d(1.0f, 1.0f) / l.vEnd;
+			vf2d invdir = vf2d(1.0f, 1.0f) / l.vEnd;
 
-			def::vf2d t_near = (r.vStart - l.vStart) * invdir;
-			def::vf2d t_far = (r.vEnd - l.vStart) * invdir;
+			vf2d t_near = (r.vStart - l.vStart) * invdir;
+			vf2d t_far = (r.vEnd - l.vStart) * invdir;
 
 			if (isnan(t_far.y) || isnan(t_far.x)) return false;
 			if (isnan(t_near.y) || isnan(t_near.x)) return false;
@@ -104,7 +106,7 @@ namespace def
 			if (t_hit_far < 0)
 				return false;
 
-			contact_point = l.vStart + def::vf2d(t_hit_near, t_hit_near) * l.vEnd;
+			contact_point = l.vStart + vf2d(t_hit_near, t_hit_near) * l.vEnd;
 
 			if (t_near.x > t_near.y)
 				if (invdir.x < 0)
@@ -120,10 +122,118 @@ namespace def
 			return true;
 		}
 
-		bool ResolveLineVsRect(const sLine& l, const sRectangle& r, def::vf2d& contact_point, def::vf2d& contact_normal)
+		bool ResolveLineVsRect(const sLine& l, const sRectangle& r, vf2d& contact_point, vf2d& contact_normal)
 		{
 			float t_hit_near;
 			return LineVsRect(l, r, contact_point, contact_normal, t_hit_near) && t_hit_near < 0.9f && t_hit_near > 0.0f;
+		}
+
+		bool DynamicRectVsRect(const sRectangle& r_dynamic, const float fTimeStep, const sRectangle& r_static,
+			vf2d& contact_point, vf2d& contact_normal, float& contact_time)
+		{
+			if (r_dynamic.vVelocity.x == 0 && r_dynamic.vVelocity.y == 0)
+				return false;
+
+			sRectangle expanded_target;
+
+			expanded_target.vStart = r_static.vStart - (r_dynamic.vEnd - r_dynamic.vStart) / 2;
+			expanded_target.vEnd = expanded_target.vStart + (r_static.vEnd - r_static.vStart) + (r_dynamic.vEnd - r_dynamic.vStart);
+
+			sLine l;
+			l.vStart = r_dynamic.vStart + (r_dynamic.vEnd - r_dynamic.vStart) / 2;
+			l.vEnd = r_dynamic.vVelocity * fTimeStep;
+
+			if (LineVsRect(l, expanded_target, contact_point, contact_normal, contact_time))
+				return (contact_time >= 0.0f && contact_time < 1.0f);
+			else
+				return false;
+		}
+
+		bool ResolveDynamicRectVsRect(sRectangle& r_dynamic, const float fTimeStep, sRectangle* r_static)
+		{
+			vf2d contact_point, contact_normal;
+			float contact_time = 0.0f;
+
+			if (DynamicRectVsRect(r_dynamic, fTimeStep, *r_static, contact_point, contact_normal, contact_time))
+			{
+				if (contact_normal.y > 0) r_dynamic.contact[0] = r_static; else nullptr;
+				if (contact_normal.x < 0) r_dynamic.contact[1] = r_static; else nullptr;
+				if (contact_normal.y < 0) r_dynamic.contact[2] = r_static; else nullptr;
+				if (contact_normal.x > 0) r_dynamic.contact[3] = r_static; else nullptr;
+
+				r_dynamic.vVelocity += contact_normal * r_dynamic.vVelocity.abs() * (1 - contact_time);
+
+				return true;
+			}
+
+			return false;
+		}
+
+	private:
+		std::vector<sPoint> vecPoints;
+		std::vector<sRectangle> vecRectangles;
+		std::vector<sCircle> vecCircles;
+		std::vector<sLine> vecLines;
+
+	public:
+		std::vector<sPoint>* GetPoints()
+		{
+			return &vecPoints;
+		}
+
+		std::vector<sRectangle>* GetRectangles()
+		{
+			return &vecRectangles;
+		}
+
+		std::vector<sCircle>* GetCircles()
+		{
+			return &vecCircles;
+		}
+
+		std::vector<sLine>* GetLines()
+		{
+			return &vecLines;
+		}
+
+		void AddPoint(sPoint p)
+		{
+			return vecPoints.push_back(p);
+		}
+
+		void AddRectangle(sRectangle r)
+		{
+			return vecRectangles.push_back(r);
+		}
+
+		void AddCircle(sCircle c)
+		{
+			return vecCircles.push_back(c);
+		}
+
+		void AddLine(sLine l)
+		{
+			return vecLines.push_back(l);
+		}
+
+		sPoint& GetPoint(uint32_t id)
+		{
+			return vecPoints[id];
+		}
+
+		sRectangle& GetRectangle(uint32_t id)
+		{
+			return vecRectangles[id];
+		}
+
+		sCircle& GetCircle(uint32_t id)
+		{
+			return vecCircles[id];
+		}
+
+		sLine& GetLine(uint32_t id)
+		{
+			return vecLines[id];
 		}
 	};
 }
