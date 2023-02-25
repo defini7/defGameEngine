08ef78e9754f4cad3a93d6d53a4195aaa0a71846
Alex <75216201+defini7@users.noreply.github.com>
2023-02-25 17:39:00 +0300

    Update WebCam.cpp

--- Examples/WebCam.cpp
+++ Examples/WebCam.cpp
@@ -1,102 +1,383 @@
 #include "defGameEngine.h"
 #include "escapi.h"
 
+#include <algorithm>
+
+constexpr int FRAME_WIDTH = 320;
+constexpr int FRAME_HEIGHT = 240;
+
+struct Frame
+{
+	float* pixels = nullptr;
+
+	Frame()
+	{
+		pixels = new float[FRAME_WIDTH * FRAME_HEIGHT];
+	}
+
+	~Frame()
+	{
+		delete[] pixels;
+	}
+
+	float get(int x, int y)
+	{
+		if (x < 0 || y < 0 || x >= FRAME_WIDTH || y >= FRAME_HEIGHT)
+			return 0.0f;
+
+		return pixels[y * FRAME_WIDTH + x];
+	}
+
+	void set(int x, int y, float p)
+	{
+		if (x < 0 || y < 0 || x >= FRAME_WIDTH || y >= FRAME_HEIGHT)
+			return;
+
+		pixels[y * FRAME_WIDTH + x] = p;
+	}
+
+	void operator=(const Frame& f)
+	{
+		memcpy(pixels, f.pixels, FRAME_WIDTH * FRAME_HEIGHT * sizeof(float));
+	}
+
+};
+
 class WebCam : public def::GameEngine
 {
 public:
 	WebCam()
 	{
 		SetTitle("Web Cam");
+		ShowFPS();
 	}
 
 private:
-	int nCameras = 0;
-	SimpleCapParams scpCapture;
+	SimpleCapParams capture;
+
+	Frame input, output, prevInput;
+
+	union RGBint
+	{
+		int rgb;
+		uint8_t c[4];
+	};
+
+	enum class Filter
+	{
+		None,
+		Threshold,
+		Motion,
+		Convolution,
+		LowPass,
+		Adaptive,
+		Sobel,
+		Median
+	};
+
+	Filter filter = Filter::None;
+
+	float fThreshold = 0.5f;
+	float fLowPass = 0.1f;
+	float fAdaptive = 0.5f;
+
+	float* fConvoKernel = fConvoRidgeKernel;
+
+	float fConvoRidgeKernel[9] =
+	{
+		0.0f, -1.0f, 0.0f,
+		-1.0f, 4.0f, -1.0f,
+		0.0f, -1.0f, 0.0f
+	};
+
+	float fConvoEdgeKernel[9] =
+	{
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  8.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f
+	};
+
+	float fConvoSharpenKernel[9] =
+	{
+		0.0f, -1.0f, 0.0f,
+		-1.0f, 5.0f, -1.0f,
+		0.0f, -1.0f, 0.0f
+	};
+
+	float fConvoBlurKernel[9] =
+	{
+		1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+		1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+		1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f
+	};
+
+	float fSobelKernelX[9] =
+	{
+		-1.0f, 0.0f, 1.0f,
+		-2.0f, 0.0f, 2.0f,
+		-1.0f, 0.0f, 1.0f
+	};
+
+	float fSobelKernelY[9] =
+	{
+		-1.0f, -2.0f, -1.0f,
+		0.0f, 0.0f, 0.0f,
+		1.0f, 2.0f, 1.0f
+	};
 
 protected:
-	void ApplyThreshold(def::Pixel& p)
+	void DrawFrame(Frame& frame, int x, int y)
 	{
-		if (p.g > 122)
-		{
-			p.r = 255;
-			p.g = 255;
-			p.b = 255;
-		}
-		else
-		{
-			p.r = 0;
-			p.g = 0;
-			p.b = 0;
-		}
-	}
+		for (int i = 0; i < FRAME_WIDTH; i++)
+			for (int j = 0; j < FRAME_HEIGHT; j++)
+			{
+				uint8_t c = (uint8_t)std::min(std::max(0.0f, frame.pixels[j * FRAME_WIDTH + i] * 255.0f), 255.0f);
 
-	void ApplySepia(def::Pixel& p)
-	{
-		int sr = int(0.393f * p.r + 0.769f * p.g + 0.189f * p.b);
-		int sg = int(0.349f * p.r + 0.686f * p.g + 0.168f * p.b);
-		int sb = int(0.272f * p.r + 0.534f * p.g + 0.131f * p.b);
-
-		p.r = (sr > 255) ? 255 : sr;
-		p.g = (sg > 255) ? 255 : sg;
-		p.b = (sb > 255) ? 255 : sb;
-	}
-
-	void ApplyGreyscale(def::Pixel& p)
-	{
-		int n = (p.r + p.g + p.b) / 3;
-
-		p.r = n;
-		p.g = n;
-		p.b = n;
+				Draw(x + i, y + j, def::Pixel(c, c, c));
+			}
 	}
 
 	bool OnUserCreate() override
 	{
-		nCameras = setupESCAPI();
-
-		if (nCameras == 0)
+		if (setupESCAPI() == 0)
 			return false;
 
-		scpCapture.mWidth = GetScreenWidth();
-		scpCapture.mHeight = GetScreenHeight();
-		
-		scpCapture.mTargetBuf = new int[GetScreenWidth() * GetScreenHeight()];
+		capture.mWidth = FRAME_WIDTH;
+		capture.mHeight = FRAME_HEIGHT;
 
-		if (!initCapture(0, &scpCapture))
+		capture.mTargetBuf = new int[FRAME_WIDTH * FRAME_HEIGHT];
+
+		if (!initCapture(0, &capture))
 			return false;
-		
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fDeltaTime) override
 	{
+		prevInput = input;
+
 		doCapture(0);
+		while (isCaptureDone(0) == 0);
 
-		for (int x = 0; x < scpCapture.mWidth; x++)
-			for (int y = 0; y < scpCapture.mHeight; y++)
+		for (int y = 0; y < capture.mHeight; y++)
+			for (int x = 0; x < capture.mWidth; x++)
 			{
-				union RGBAint
-				{
-					int rgba;
-					uint8_t c[4];
-				};
+				int i = y * capture.mWidth + x;
 
-				RGBAint col;
-				col.rgba = scpCapture.mTargetBuf[y * scpCapture.mWidth + x];
+				RGBint col;
+				col.rgb = capture.mTargetBuf[i];
 
-				def::Pixel p = def::Pixel(col.c[2], col.c[1], col.c[0], 255);
-
-				if (GetKey(def::Key::G).bHeld)
-					ApplyGreyscale(p);
-
-				if (GetKey(def::Key::S).bHeld)
-					ApplySepia(p);
-
-				if (GetKey(def::Key::T).bHeld)
-					ApplyThreshold(p);
-
-				Draw(x, y, p);
+				input.pixels[i] = (float)col.c[1] / 255.0f;
 			}
+
+		if (GetKey(def::Key::K1).bPressed) filter = Filter::None;
+		if (GetKey(def::Key::K2).bPressed) filter = Filter::Threshold;
+		if (GetKey(def::Key::K3).bPressed) filter = Filter::Motion;
+		if (GetKey(def::Key::K4).bPressed) filter = Filter::Convolution;
+		if (GetKey(def::Key::K5).bPressed) filter = Filter::LowPass;
+		if (GetKey(def::Key::K6).bPressed) filter = Filter::Sobel;
+		if (GetKey(def::Key::K7).bPressed) filter = Filter::Median;
+		if (GetKey(def::Key::K8).bPressed) filter = Filter::Adaptive;
+
+		switch (filter)
+		{
+		case Filter::None:
+		{
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+					output.set(x, y, input.get(x, y));
+		}
+		break;
+
+		case Filter::Threshold:
+		{
+			if (GetKey(def::Key::Z).bHeld) fThreshold -= 0.1f * fDeltaTime;
+			if (GetKey(def::Key::X).bHeld) fThreshold += 0.1f * fDeltaTime;
+
+			fThreshold = std::min(1.0f, std::max(fThreshold, 0.0f));
+
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+					output.set(x, y, input.get(x, y) >= fThreshold ? 1.0f : 0.0f);
+		}
+		break;
+
+		case Filter::Motion:
+		{
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+					output.set(x, y, fabs(input.get(x, y) - prevInput.get(x, y)));
+		}
+		break;
+
+		case Filter::Convolution:
+		{
+			if (GetKey(def::Key::Z).bHeld) fConvoKernel = fConvoRidgeKernel;
+			if (GetKey(def::Key::X).bHeld) fConvoKernel = fConvoEdgeKernel;
+			if (GetKey(def::Key::C).bHeld) fConvoKernel = fConvoSharpenKernel;
+			if (GetKey(def::Key::V).bHeld) fConvoKernel = fConvoBlurKernel;
+
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+				{
+					float fSum = 0.0f;
+
+					for (int i = 0; i < 3; i++)
+						for (int j = 0; j < 3; j++)
+							fSum += input.get(x + i - 1, y + j - 1) * fConvoKernel[j * 3 + i];
+
+					output.set(x, y, fSum);
+				}
+		}
+		break;
+
+		case Filter::LowPass:
+		{
+			if (GetKey(def::Key::Z).bHeld) fLowPass -= 0.1f * fDeltaTime;
+			if (GetKey(def::Key::X).bHeld) fLowPass += 0.1f * fDeltaTime;
+
+			fLowPass = std::min(1.0f, std::max(fLowPass, 0.0f));
+
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+				{
+					float fDist = input.get(x, y) - output.get(x, y);
+					fDist *= fLowPass;
+					output.set(x, y, fDist + output.get(x, y));
+				}
+		}
+		break;
+
+		case Filter::Adaptive:
+		{
+			if (GetKey(def::Key::Z).bHeld) fAdaptive -= 0.1f * fDeltaTime;
+			if (GetKey(def::Key::X).bHeld) fAdaptive += 0.1f * fDeltaTime;
+
+			fAdaptive = std::min(1.0f, std::max(fAdaptive, 0.5f));
+
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+				{
+					float fSum = 0.0f;
+
+					for (int i = -2; i <= 2; i++)
+						for (int j = -2; j <= 2; j++)
+							fSum += input.get(x + i, y + j);
+
+					fSum /= 25.0f;
+					output.set(x, y, input.get(x, y) > (fSum* fAdaptive) ? 1.0f : 0.0f);
+				}
+		}
+		break;
+
+		case Filter::Sobel:
+		{
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+				{
+					float fSumX = 0.0f, fSumY = 0.0f;
+
+					for (int i = 0; i < 3; i++)
+						for (int j = 0; j < 3; j++)
+						{
+							fSumX += input.get(x + i - 1, y + j - 1) * fSobelKernelX[j * 3 + i];
+							fSumY += input.get(x + i - 1, y + j - 1) * fSobelKernelY[j * 3 + i];
+						}
+
+					output.set(x, y, fabs((fSumX + fSumY) / 2.0f));
+				}
+		}
+		break;
+
+		case Filter::Median:
+		{
+			for (int x = 0; x < FRAME_WIDTH; x++)
+				for (int y = 0; y < FRAME_HEIGHT; y++)
+				{
+					std::vector<float> col;
+
+					for (int i = -2; i <= 2; i++)
+						for (int j = -2; j <= 2; j++)
+							col.push_back(input.get(x + i, y + j));
+
+					std::sort(col.begin(), col.end(), std::greater<float>());
+
+					output.set(x, y, col[4]);
+				}
+
+		}
+		break;
+
+		}
+
+		Clear(def::DARK_BLUE);
+
+		DrawFrame(input, 10, 10);
+		DrawFrame(output, 470, 10);
+
+		switch (filter)
+		{
+		case Filter::None:
+			DrawString(50, 300, "Filter: None");
+		break;
+
+		case Filter::Threshold:
+		{
+			DrawString(50, 300, "Filter: Threshold");
+			DrawString(50, 316, "Change threshold value with Z and X keys");
+			DrawString(50, 332, "Current value = " + std::to_string(fThreshold));
+		}
+		break;
+
+		case Filter::Motion:
+		{
+			DrawString(50, 300, "Filter: Motion");
+		}
+		break;
+
+		case Filter::Convolution:
+		{
+			DrawString(50, 300, "Filter: Convolution");
+			DrawString(50, 316, "Change convolution filter with Z, X, C, V keys");
+		}
+		break;
+
+		case Filter::LowPass:
+		{
+			DrawString(50, 300, "Filter: LowPass");
+			DrawString(50, 316, "Change lowpass value with Z and X keys");
+			DrawString(50, 332, "Current value = " + std::to_string(fLowPass));
+		}
+		break;
+
+		case Filter::Adaptive:
+		{
+			DrawString(50, 300, "Filter: Adaptive Threshold");
+			DrawString(50, 316, "Change adaptive threshold value with Z and X keys");
+			DrawString(50, 332, "Current value = " + std::to_string(fAdaptive));
+		}
+		break;
+
+		case Filter::Sobel:
+			DrawString(50, 300, "Filter: Sobel");
+		break;
+
+		case Filter::Median:
+			DrawString(50, 300, "Filter: Median");
+		break;
+
+		}
+
+		DrawString(500, 300, "Available filters: ");
+		DrawString(500, 316, "1) None");
+		DrawString(500, 332, "2) Threshold");
+		DrawString(500, 348, "3) Motion");
+		DrawString(500, 364, "4) Convolution");
+		DrawString(500, 380, "5) LowPass");
+		DrawString(500, 396, "6) Adaptive");
+		DrawString(500, 412, "7) Sobel");
+		DrawString(500, 428, "8) Median");
 
 		return true;
 	}
@@ -106,7 +387,7 @@ protected:
 int main()
 {
 	WebCam demo;
-	def::rcode err = demo.Construct(256, 240, 4, 4);
+	def::rcode err = demo.Construct(800, 500, 2, 2);
 
 	if (err.ok)
 		demo.Run();
