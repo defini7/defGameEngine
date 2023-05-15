65fd860a830c08d015a56cf7bc9b193698f22f8a
Alex <75216201+defini7@users.noreply.github.com>
2023-05-15 20:54:23 +0300

    Update Mode7.cpp

--- Examples/Mode7.cpp
+++ Examples/Mode7.cpp
@@ -1,3 +1,4 @@
+#define DGE_APPLICATION
 #include "defGameEngine.h"
 
 class Mode7 : public def::GameEngine
@@ -40,20 +41,20 @@ public:
 		vCamera += vVelocity * fDeltaTime;
 
 		def::vf2d vScreen;
-		for (vScreen.x = 0; vScreen.x < GetScreenWidth(); vScreen.x++)
+		for (vScreen.x = 0; vScreen.x < ScreenWidth(); vScreen.x++)
 		{
-			for (vScreen.y = GetScreenHeight() / 2; vScreen.y < GetScreenHeight(); vScreen.y++)
+			for (vScreen.y = ScreenHeight() / 2; vScreen.y < ScreenHeight(); vScreen.y++)
 			{
-				def::vf2d vWindow = def::vf2d(GetScreenSize()) / 2.0f + vScreen * def::vf2d(-1.0f, 1.0f);
-				float fWindowZ = vScreen.y - GetScreenHeight() / 2;
+				def::vf2d vWindow = def::vf2d(ScreenSize()) / 2.0f + vScreen * def::vf2d(-1.0f, 1.0f);
+				float fWindowZ = vScreen.y - ScreenHeight() / 2;
 
 				float fRotatedX = vWindow.x * cos(fTheta) - vWindow.y * sin(fTheta);
 				float fRotatedY = vWindow.x * sin(fTheta) + vWindow.y * cos(fTheta);
-				
+
 				def::vi2d vPixel = (def::vf2d(fRotatedX, fRotatedY) / fWindowZ + vCamera) * vScale;
 
-				def::Pixel pixKart = sprKart->GetPixel(vPixel.x % sprKart->GetWidth(), vPixel.y % sprKart->GetHeight());
-				def::Pixel pixSky = sprSky->GetPixel(vPixel.x % sprSky->GetWidth(), vPixel.y % sprSky->GetHeight());
+				def::Pixel pixKart = sprKart->GetPixel(vPixel.x % sprKart->nWidth, vPixel.y % sprKart->nHeight);
+				def::Pixel pixSky = sprSky->GetPixel(vPixel.x % sprSky->nWidth, vPixel.y % sprSky->nHeight);
 
 				//float fShade = std::min(std::max(1.5f * (abs(fWindowZ) / (GetScreenHeight() / 2)), 0.0f), 1.0f);
 
@@ -66,11 +67,11 @@ public:
 				//pixSky.b = (float)pixSky.b * fShade;
 
 				Draw(def::vf2d(vScreen.x, vScreen.y), pixKart);
-				Draw(def::vf2d(vScreen.x, GetScreenHeight() - vScreen.y - 1), pixSky);
+				Draw(def::vf2d(vScreen.x, ScreenHeight() - vScreen.y - 1), pixSky);
 			}
 		}
 
-		for (int x = 0; x < GetScreenWidth(); x++) Draw(x, GetScreenHeight() / 2, def::CYAN);
+		for (int x = 0; x < ScreenWidth(); x++) Draw(x, ScreenHeight() / 2, def::CYAN);
 
 		return true;
 	}
