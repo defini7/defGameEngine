c52399fed81d2e7440286883c12fcad844deef82
Alex <75216201+defini7@users.noreply.github.com>
2023-06-16 09:21:23 +0300

    Update defGameEngine.h

--- defGameEngine.h
+++ defGameEngine.h
@@ -3,7 +3,7 @@
 #pragma region license
 /***
 *	BSD 3-Clause License
-	Copyright (c) 2021, 2022, 2023 Alex
+	Copyright (c) 2022, 2023 Alex
 	All rights reserved.
 	Redistribution and use in source and binary forms, with or without
 	modification, are permitted provided that the following conditions are met:
