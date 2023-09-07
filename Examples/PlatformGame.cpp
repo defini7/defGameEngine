659d3127cb8b77c5336ed2347295763d633208ae
Alex <75216201+defini7@users.noreply.github.com>
2023-09-07 18:33:29 +0300

    Update PlatformGame.cpp

--- Examples/PlatformGame.cpp
+++ Examples/PlatformGame.cpp
@@ -23,19 +23,20 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
+#define DGE_APPLICATION
+#include "defGameEngine.hpp"
 
 #include <unordered_map>
+#include <functional>
 
 using namespace std;
 
-class Mario : public olc::PixelGameEngine
+class Mario : public def::GameEngine
 {
 public:
 	Mario()
 	{
-		sAppName = "Mario";
+		SetTitle("Mario");
 	}
 
 	~Mario()
@@ -45,7 +46,7 @@ public:
 	}
 
 private:
-	olc::vi2d vTileSize = { 32, 32 };
+	def::vi2d vTileSize = { 32, 32 };
 
 	enum class TileType
 	{
@@ -57,7 +58,7 @@ private:
 
 	struct Level
 	{
-		olc::vi2d vSize;
+		def::vi2d vSize;
 		vector<TileType> vecData;
 	};
 
@@ -66,17 +67,17 @@ private:
 	vector<Level> vecLevels;
 	size_t nCurrentLevel = 0;
 
-	unordered_map<TileType, olc::vi2d> mapSpriteFileOffsets;
+	unordered_map<TileType, def::vi2d> mapSpriteFileOffsets;
 
-	olc::vf2d vPlayerPos = { 1.0f, 1.0f };
-	olc::vf2d vPlayerVel = { 0.0f, 0.0f };
+	def::vf2d vPlayerPos = { 1.0f, 1.0f };
+	def::vf2d vPlayerVel = { 0.0f, 0.0f };
 
 	bool bIsPlayerOnGround = false;
 
-	olc::Sprite* sprMario = nullptr;
-	olc::Sprite* sprBackground = nullptr;
+	def::Sprite* sprMario = nullptr;
+	def::Sprite* sprBackground = nullptr;
 
-	olc::vi2d vGraphicsID;
+	def::vi2d vGraphicsID;
 	int nScore = 0;
 
 	enum GameState
@@ -94,14 +95,14 @@ private:
 
 protected:
 	// That doesn't modify nCurrentLevel, just pushes a new level at the end of the vector
-	void LoadLevel(const std::string& sMap, const olc::vi2d& vSize, std::unordered_map<char, TileType> mapLookUp)
+	void LoadLevel(const std::string& sMap, const def::vi2d& vSize, std::unordered_map<char, TileType> mapLookUp)
 	{
 		CreateLevel(vSize);
 
 		size_t nSaveLevel = nCurrentLevel;
 		nCurrentLevel = vecLevels.size() - 1;
 
-		olc::vi2d vTile;
+		def::vi2d vTile;
 		for (vTile.y = 0; vTile.y < vSize.y; vTile.y++)
 			for (vTile.x = 0; vTile.x < vSize.x; vTile.x++)
 			{
@@ -112,7 +113,7 @@ protected:
 		nCurrentLevel = nSaveLevel;
 	}
 
-	void CreateLevel(const olc::vi2d& vSize)
+	void CreateLevel(const def::vi2d& vSize)
 	{
 		Level level;
 		level.vSize = vSize;
@@ -122,7 +123,7 @@ protected:
 	}
 
 	// Works with vecLevels[nCurrentLevel]
-	void SetTile(const olc::vi2d& p, TileType tile)
+	void SetTile(const def::vi2d& p, TileType tile)
 	{
 		auto& level = vecLevels[nCurrentLevel];
 
@@ -131,7 +132,7 @@ protected:
 	}
 
 	// Works with vecLevels[nCurrentLevel]
-	TileType GetTile(const olc::vi2d& p)
+	TileType GetTile(const def::vi2d& p)
 	{
 		auto& level = vecLevels[nCurrentLevel];
 
@@ -154,7 +155,7 @@ protected:
 	{
 		if (IsFocused())
 		{
-			if (GetKey(olc::Key::LEFT).bHeld)
+			if (GetKey(def::Key::LEFT).held)
 			{
 				vPlayerVel.x += (bIsPlayerOnGround ? -25.0f : -15.0f) * fDeltaTime;
 
@@ -165,7 +166,7 @@ protected:
 				}
 			}
 
-			if (GetKey(olc::Key::RIGHT).bHeld)
+			if (GetKey(def::Key::RIGHT).held)
 			{
 				vPlayerVel.x += (bIsPlayerOnGround ? 25.0f : 15.0f) * fDeltaTime;
 
@@ -176,11 +177,17 @@ protected:
 				}
 			}
 
-			if (GetKey(olc::Key::SPACE).bPressed)
+			if (GetKey(def::Key::SPACE).pressed)
 			{
 				if (vPlayerVel.y == 0.0f) vPlayerVel.y = -12.0f;
 				vGraphicsID = { (vPlayerVel.x >= 0.0f ? 0 : 1), 2 };
 			}
+		}
+
+		if (bIsPlayerOnGround)
+		{
+			if (vPlayerVel == def::vf2d())
+				vGraphicsID.y = 0;
 		}
 
 		fFrameCounter += fDeltaTime;
@@ -188,7 +195,7 @@ protected:
 
 	void UpdateCollision(float fDeltaTime)
 	{
-		olc::vf2d vNewPlayerPos = vPlayerPos + vPlayerVel * fDeltaTime;
+		def::vf2d vNewPlayerPos = vPlayerPos + vPlayerVel * fDeltaTime;
 
 		if (GetTile(vNewPlayerPos) == TileType::Coin)
 		{
@@ -196,27 +203,27 @@ protected:
 			nScore++;
 		}
 
-		if (GetTile(vNewPlayerPos + olc::vf2d(0.0f, 1.0f)) == TileType::Coin)
+		if (GetTile(vNewPlayerPos + def::vf2d(0.0f, 1.0f)) == TileType::Coin)
 		{
-			SetTile(vNewPlayerPos + olc::vf2d(0.0f, 1.0f), TileType::Empty);
+			SetTile(vNewPlayerPos + def::vf2d(0.0f, 1.0f), TileType::Empty);
 			nScore++;
 		}
 
-		if (GetTile(vNewPlayerPos + olc::vf2d(1.0f, 0.0f)) == TileType::Coin)
+		if (GetTile(vNewPlayerPos + def::vf2d(1.0f, 0.0f)) == TileType::Coin)
 		{
-			SetTile(vNewPlayerPos + olc::vf2d(1.0f, 0.0f), TileType::Empty);
+			SetTile(vNewPlayerPos + def::vf2d(1.0f, 0.0f), TileType::Empty);
 			nScore++;
 		}
 
-		if (GetTile(vNewPlayerPos + olc::vf2d(1.0f, 1.0f)) == TileType::Coin)
+		if (GetTile(vNewPlayerPos + def::vf2d(1.0f, 1.0f)) == TileType::Coin)
 		{
-			SetTile(vNewPlayerPos + olc::vf2d(1.0f, 1.0f), TileType::Empty);
+			SetTile(vNewPlayerPos + def::vf2d(1.0f, 1.0f), TileType::Empty);
 			nScore++;
 		}
 
 		if (vPlayerVel.x <= 0.0f)
 		{
-			if (GetTile(olc::vi2d(vNewPlayerPos.x + 0.0f, vPlayerPos.y + 0.0f)) != TileType::Empty || GetTile(olc::vi2d(vNewPlayerPos.x + 0.0f, vPlayerPos.y + 0.9f)) != TileType::Empty)
+			if (GetTile(def::vi2d(vNewPlayerPos.x + 0.0f, vPlayerPos.y + 0.0f)) != TileType::Empty || GetTile(def::vi2d(vNewPlayerPos.x + 0.0f, vPlayerPos.y + 0.9f)) != TileType::Empty)
 			{
 				vNewPlayerPos.x = floor(vNewPlayerPos.x) + 1.0f;
 				vPlayerVel.x = 0.0f;
@@ -224,7 +231,7 @@ protected:
 		}
 		else
 		{
-			if (GetTile(olc::vi2d(vNewPlayerPos.x + 1.0f, vPlayerPos.y + 0.0f)) != TileType::Empty || GetTile(olc::vi2d(vNewPlayerPos.x + 1.0f, vPlayerPos.y + 0.9f)) != TileType::Empty)
+			if (GetTile(def::vi2d(vNewPlayerPos.x + 1.0f, vPlayerPos.y + 0.0f)) != TileType::Empty || GetTile(def::vi2d(vNewPlayerPos.x + 1.0f, vPlayerPos.y + 0.9f)) != TileType::Empty)
 			{
 				vNewPlayerPos.x = floor(vNewPlayerPos.x);
 				vPlayerVel.x = 0.0f;
@@ -234,7 +241,7 @@ protected:
 		bIsPlayerOnGround = false;
 		if (vPlayerVel.y <= 0.0f)
 		{
-			if (GetTile(vNewPlayerPos) != TileType::Empty || GetTile(vNewPlayerPos + olc::vf2d(0.9f, 0.0f)) != TileType::Empty)
+			if (GetTile(vNewPlayerPos) != TileType::Empty || GetTile(vNewPlayerPos + def::vf2d(0.9f, 0.0f)) != TileType::Empty)
 			{
 				vNewPlayerPos.y = floor(vNewPlayerPos.y) + 1.0f;
 				vPlayerVel.y = 0.0f;
@@ -242,7 +249,7 @@ protected:
 		}
 		else
 		{
-			if (GetTile(vNewPlayerPos + olc::vf2d(0.0f, 1.0f)) != TileType::Empty || GetTile(vNewPlayerPos + olc::vf2d(0.9f, 1.0f)) != TileType::Empty)
+			if (GetTile(vNewPlayerPos + def::vf2d(0.0f, 1.0f)) != TileType::Empty || GetTile(vNewPlayerPos + def::vf2d(0.9f, 1.0f)) != TileType::Empty)
 			{
 				vNewPlayerPos.y = floor(vNewPlayerPos.y);
 				vPlayerVel.y = 0.0f;
@@ -302,33 +309,33 @@ protected:
 	{
 		DrawSprite(0, 0, sprBackground);
 
-		olc::vi2d vVisibleTiles = GetScreenSize() / vTileSize;
-		olc::vf2d vOffset = vPlayerPos - (olc::vf2d)vVisibleTiles * 0.5f;
+		def::vi2d vVisibleTiles = ScreenSize() / vTileSize;
+		def::vf2d vOffset = vPlayerPos - (def::vf2d)vVisibleTiles * 0.5f;
 
 		vOffset = vOffset.max({ 0.0f, 0.0f });
 		vOffset = vOffset.min(vecLevels[nCurrentLevel].vSize - vVisibleTiles);
 
-		olc::vf2d vTileOffset = (vOffset - vOffset.floor()) * (olc::vf2d)vTileSize;
+		def::vf2d vTileOffset = (vOffset - vOffset.floor()) * (def::vf2d)vTileSize;
 
-		SetPixelMode(olc::Pixel::ALPHA);
+		SetPixelMode(def::Pixel::ALPHA);
 
-		olc::vi2d vTile;
+		def::vi2d vTile;
 		for (vTile.y = -1; vTile.y < vVisibleTiles.y + 1; vTile.y++)
 		{
 			for (vTile.x = -1; vTile.x < vVisibleTiles.x + 1; vTile.x++)
 			{
-				TileType tile = GetTile(vTile + (olc::vi2d)vOffset);
+				TileType tile = GetTile(vTile + (def::vi2d)vOffset);
 
-				olc::vi2d p = olc::vf2d(vTile * vTileSize) - vTileOffset;
+				def::vi2d p = def::vf2d(vTile * vTileSize) - vTileOffset;
 
 				if (tile != TileType::Empty)
-					DrawPartialSprite(p, sprMario, mapSpriteFileOffsets[tile], vTileSize);
+					DrawPartialSprite(p, mapSpriteFileOffsets[tile], vTileSize, sprMario);
 			}
 		}
 
-		DrawPartialSprite((vPlayerPos - vOffset) * (olc::vf2d)vTileSize, sprMario, vTileSize * vGraphicsID, vTileSize);
+		DrawPartialSprite((vPlayerPos - vOffset) * (def::vf2d)vTileSize, vTileSize * vGraphicsID, vTileSize, sprMario);
 	
-		SetPixelMode(olc::Pixel::NORMAL);
+		SetPixelMode(def::Pixel::DEFAULT);
 	}
 
 	void DrawInterface()
@@ -338,7 +345,7 @@ protected:
 
 	void State_Menu()
 	{
-		if (GetKey(olc::Key::ESCAPE).bPressed)
+		if (GetKey(def::Key::ESCAPE).pressed)
 		{
 			nMenuCursor = 0;
 			nState = GameState::Game;
@@ -363,41 +370,41 @@ protected:
 			[&]() { bRunning = false; }
 		};
 
-		Clear(olc::BLUE);
+		Clear(def::BLUE);
 
-		if (GetKey(olc::Key::UP).bPressed)
+		if (GetKey(def::Key::UP).pressed)
 		{
 			nMenuCursor--;
 			if (nMenuCursor < 0) nMenuCursor = mapOptions.size() - 1;
 		}
 
-		if (GetKey(olc::Key::DOWN).bPressed)
+		if (GetKey(def::Key::DOWN).pressed)
 		{
 			nMenuCursor++;
 			if (nMenuCursor >= mapOptions.size()) nMenuCursor = 0;
 		}
 
-		if (GetKey(olc::Key::ENTER).bPressed)
+		if (GetKey(def::Key::ENTER).pressed)
 			vecOptions[nMenuCursor]();
 
 		for (const auto& [sOptionName, nOptionIndex] : mapOptions)
 		{
-			olc::vi2d vOffset = { 10, 20 + (GetTextSize(sOptionName).y + 2) * (int)nOptionIndex };
+			def::vi2d vOffset = { 10, 20 + 12 * (int)nOptionIndex };
 
 			if (nMenuCursor == nOptionIndex)
 			{
 				std::string sPointer = "-> ";
 				DrawString(vOffset, sPointer);
-				vOffset.x += GetTextSize(sPointer).x;
+				vOffset.x += 10 * sPointer.size();
 			}
 
-			DrawString(vOffset, sOptionName, olc::WHITE);
+			DrawString(vOffset, sOptionName, def::WHITE);
 		}
 	}
 
 	void State_Game(float fDeltaTime)
 	{
-		if (GetKey(olc::Key::ESCAPE).bPressed)
+		if (GetKey(def::Key::ESCAPE).pressed)
 			nState = GameState::Menu;
 
 		UpdateControls(fDeltaTime);
@@ -410,8 +417,8 @@ protected:
 
 	virtual bool OnUserCreate() override
 	{
-		sprMario = new olc::Sprite("Assets/mario.png");
-		sprBackground = new olc::Sprite("Assets/sky.png");
+		sprMario = new def::Sprite("Assets/mario.png");
+		sprBackground = new def::Sprite("Assets/sky.png");
 
 		mapSpriteFileOffsets =
 		{
@@ -469,7 +476,7 @@ protected:
 int main()
 {
 	Mario demo;
-	if (demo.Construct(256, 192, 4, 4))
-		demo.Start();
+	demo.Construct(256, 192, 4, 4);
+	demo.Run();
 	return 0;
 }
