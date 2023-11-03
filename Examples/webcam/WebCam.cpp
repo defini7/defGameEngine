3e17edc437d82ff23cc90ac8437c0a855e16d484
Alex <75216201+defini7@users.noreply.github.com>
2023-11-03 22:50:43 +0300

    Update WebCam.cpp

--- Examples/webcam/WebCam.cpp
+++ Examples/webcam/WebCam.cpp
@@ -1,34 +1,38 @@
 /*
-* BSD 3-Clause License
-Copyright (c) 2023, Alex
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*	BSD 3-Clause License
+	Copyright (c) 2023, Alex
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	1. Redistributions of source code must retain the above copyright notice, this
+	   list of conditions and the following disclaimer.
+	2. Redistributions in binary form must reproduce the above copyright notice,
+	   this list of conditions and the following disclaimer in the documentation
+	   and/or other materials provided with the distribution.
+	3. Neither the name of the copyright holder nor the names of its
+	   contributors may be used to endorse or promote products derived from
+	   this software without specific prior written permission.
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #define DGE_APPLICATION
-#include "defGameEngine.h"
+#include "defGameEngine.hpp"
 #include "escapi.h"
 
 #define DEF_IMAGE_PROCESSING
 #include "ImageProcessing.hpp"
+
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 constexpr int FRAME_WIDTH = 320;
 constexpr int FRAME_HEIGHT = 240;
@@ -47,7 +51,12 @@ void DitheringQuantise_CustomPalette(const def::img::Pixelf& in, def::img::Pixel
 
 	for (const auto& s : shades)
 	{
-		float dist = sqrt(pow(s.r - in.r, 2) + pow(s.g - in.g, 2) + pow(s.b - in.b, 2));
+		def::img::Pixelf d;
+		d.r = s.r - in.r;
+		d.g = s.g - in.g;
+		d.b = s.b - in.b;
+
+		float dist = sqrt(d.r * d.r + d.g * d.g + d.b * d.b);
 
 		if (dist < closest)
 		{
@@ -65,9 +74,11 @@ public:
 		SetTitle("Web Cam");
 	}
 
-	~WebCam()
+	virtual ~WebCam()
 	{
-		deinitCapture(0);
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
 	}
 
 private:
@@ -92,7 +103,8 @@ private:
 		Median,
 		Dithering_FloydSteinberg,
 		Dithering_Ordered,
-		Ripple
+		Ripple,
+		CRT
 	};
 
 	enum class DitheringQuantiseMode
@@ -120,12 +132,12 @@ private:
 	int bitsPerChannel = 3;
 
 	float accTime = 0.0f;
-	float rippleCenterX = 0.5f;
-	float rippleCenterY = 0.5f;
+	float rippleCenter[2] = { 0.5f, 0.5f };
 
-	// Windows Command Prompt colors
-	const std::vector<def::img::Pixelf> DITH_PALETTE = {
-		{ 12.0f / 255.0f,12.0f / 255.0f,12.0f / 255.0f },
+	// Windows Command Prompt colors by default
+	std::vector<def::img::Pixelf> vDitheringPalette =
+	{
+		{ 12.0f / 255.0f,12.0f / 255.0f, 12.0f / 255.0f },
 		{ 0.0f, 55.0f / 255.0f, 218.0f / 255.0f },
 		{ 19.0f / 255.0f, 161.0f / 255.0f, 14.0f / 255.0f },
 		{ 58.0f / 255.0f, 150.0f / 255.0f, 221.0f / 255.0f },
@@ -142,6 +154,19 @@ private:
 		{ 249.0f / 255.0f, 241.0f / 255.0f, 165.0f / 255.0f },
 		{ 242.0f / 255.0f, 242.0f / 255.0f, 242.0f / 255.0f, },
 	};
+	
+	def::img::Pixelf crtGun = { 0.0f, 0.0f, 0.0f };
+	def::img::Pixelf crtSkew = { 0.64f, 0.24f, 0.4f };
+	def::img::Pixelf crtAlign = { 1.1f, -0.8f, 0.2f };
+
+	bool convoRadio[4] = { true };
+	bool dithFloydRadio[2] = { true };
+	bool dithOrdRadio[2] = { true };
+
+	bool textEntering = false;
+
+	def::img::Pixelf customColor;
+	int customCursor = 0;
 
 protected:
 	void DrawFrame(def::img::Frame& f, int x, int y)
@@ -168,6 +193,14 @@ protected:
 		prevInput.create(FRAME_WIDTH, FRAME_HEIGHT);
 		output.create(FRAME_WIDTH, FRAME_HEIGHT);
 
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+		if (!ImGui_ImplGlfw_InitForOpenGL(GetWindow(), true)) return false;
+		if (!ImGui_ImplOpenGL3_Init("#version 130")) return false;
+
 		return true;
 	}
 
@@ -185,80 +218,110 @@ protected:
 				input.pixels[i].b = (float)col.c[0] / 255.0f;
 			}
 
-		if (GetKey(def::Key::K1).pressed) filter = Filter::Threshold;
-		if (GetKey(def::Key::K2).pressed) filter = Filter::Motion;
-		if (GetKey(def::Key::K3).pressed) filter = Filter::Convolution;
-		if (GetKey(def::Key::K4).pressed) filter = Filter::LowPass;
-		if (GetKey(def::Key::K5).pressed) filter = Filter::Sobel;
-		if (GetKey(def::Key::K6).pressed) filter = Filter::Median;
-		if (GetKey(def::Key::K7).pressed) filter = Filter::Adaptive;
-		if (GetKey(def::Key::K8).pressed) filter = Filter::Dithering_FloydSteinberg;
-		if (GetKey(def::Key::K9).pressed) filter = Filter::Dithering_Ordered;
-		if (GetKey(def::Key::K0).pressed) filter = Filter::Ripple;
+		ImGui_ImplGlfw_NewFrame();
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui::NewFrame();
 
-		if (GetKey(def::Key::SPACE).held)
-			def::img::Greyscale(input, input);
+		if (!textEntering)
+		{
+			if (GetKey(def::Key::K1).pressed) filter = Filter::Threshold;
+			if (GetKey(def::Key::K2).pressed) filter = Filter::Motion;
+			if (GetKey(def::Key::K3).pressed) filter = Filter::Convolution;
+			if (GetKey(def::Key::K4).pressed) filter = Filter::LowPass;
+			if (GetKey(def::Key::K5).pressed) filter = Filter::Sobel;
+			if (GetKey(def::Key::K6).pressed) filter = Filter::Median;
+			if (GetKey(def::Key::K7).pressed) filter = Filter::Adaptive;
+			if (GetKey(def::Key::K8).pressed) filter = Filter::Dithering_FloydSteinberg;
+			if (GetKey(def::Key::K9).pressed) filter = Filter::Dithering_Ordered;
+			if (GetKey(def::Key::K0).pressed) filter = Filter::Ripple;
+			if (GetKey(def::Key::MINUS).pressed) filter = Filter::CRT;
+
+			if (GetKey(def::Key::SPACE).held)
+				def::img::Greyscale(input, input);
+		}
+
+		Clear(def::DARK_BLUE);
+
+		auto press_radio = [](const char* label, bool* values, size_t count, size_t i)
+		{
+			if (ImGui::RadioButton(label, values[i]))
+			{
+				values[i] = true;
+				for (size_t v = 0; v < count; v++)
+				{
+					if (i != v)
+						values[v] = false;
+				}
+
+				return true;
+			}
+
+			return false;
+		};
 
 		switch (filter)
 		{
 		case Filter::Threshold:
 		{
-			if (GetKey(def::Key::Z).held) threshold -= 0.1f * dt;
-			if (GetKey(def::Key::X).held) threshold += 0.1f * dt;
-
-			threshold = std::min(1.0f, std::max(threshold, 0.0f));
-
-			Threshold(input, output, threshold);
+			ImGui::SliderFloat("Value", &threshold, 0.0f, 1.0f);
+			def::img::Threshold(input, output, threshold);
 		}
 		break;
 
 		case Filter::Convolution:
 		{
-			if (GetKey(def::Key::Z).held) convoKernel = def::img::CONVO_RIDGE_KERNEL;
-			if (GetKey(def::Key::X).held) convoKernel = def::img::CONVO_EDGE_KERNEL;
-			if (GetKey(def::Key::C).held) convoKernel = def::img::CONVO_SHARPEN_KERNEL;
-			if (GetKey(def::Key::V).held) convoKernel = def::img::CONVO_BLUR_KERNEL;
+			if (press_radio("Ridge", convoRadio, 4, 0)) convoKernel = def::img::CONVO_RIDGE_KERNEL;
+			if (press_radio("Edge", convoRadio, 4, 1)) convoKernel = def::img::CONVO_EDGE_KERNEL;
+			if (press_radio("Sharpen", convoRadio, 4, 2)) convoKernel = def::img::CONVO_SHARPEN_KERNEL;
+			if (press_radio("Blur", convoRadio, 4, 3)) convoKernel = def::img::CONVO_BLUR_KERNEL;
 
-			Convolution(input, output, convoKernel);
+			def::img::Convolution(input, output, convoKernel);
 		}
 		break;
 
 		case Filter::LowPass:
 		{
-			if (GetKey(def::Key::Z).held) lowPass -= 0.1f * dt;
-			if (GetKey(def::Key::X).held) lowPass += 0.1f * dt;
-
-			lowPass = std::min(1.0f, std::max(lowPass, 0.0f));
-
-			LowPass(input, output, lowPass);
+			ImGui::SliderFloat("Value", &lowPass, 0.0f, 1.0f);
+			def::img::LowPass(input, output, lowPass);
 		}
 		break;
 
 		case Filter::Adaptive:
 		{
-			if (GetKey(def::Key::Z).held) adaptive -= 0.1f * dt;
-			if (GetKey(def::Key::X).held) adaptive += 0.1f * dt;
-
-			adaptive = std::min(1.0f, std::max(adaptive, 0.5f));
-
-			Adaptive(input, output, adaptive);
+			ImGui::SliderFloat("Value", &adaptive, 0.5f, 1.0f);
+			def::img::Adaptive(input, output, adaptive);
 		}
 		break;
 
-		case Filter::Sobel: Sobel(input, output); break;
-		case Filter::Median: Median(input, output); break;
-		case Filter::Motion: Motion(input, prevInput, output); break;
+		case Filter::Sobel: def::img::Sobel(input, output); break;
+		case Filter::Median: def::img::Median(input, output); break;
+		case Filter::Motion: def::img::Motion(input, prevInput, output); break;
 
 		case Filter::Dithering_FloydSteinberg:
 		{
-			if (GetKey(def::Key::Z).pressed) bitsPerChannel--;
-			if (GetKey(def::Key::X).pressed) bitsPerChannel++;
-			if (GetKey(def::Key::C).pressed) quantMode = DitheringQuantiseMode::NBit;
-			if (GetKey(def::Key::V).pressed) quantMode = DitheringQuantiseMode::CustomPalette;
+			if (press_radio("NBit", dithFloydRadio, 2, 0)) quantMode = DitheringQuantiseMode::NBit;
+			if (press_radio("Custom", dithFloydRadio, 2, 1)) quantMode = DitheringQuantiseMode::CustomPalette;
 
-			bitsPerChannel = std::min(std::max(bitsPerChannel, 1), 8);
+			ImGui::SliderInt("Bits per channel", &bitsPerChannel, 1, 8);
 
-			Dithering_FloydSteinberg(input, output,
+			textEntering = (quantMode == DitheringQuantiseMode::CustomPalette);
+
+			if (textEntering)
+			{
+				ImGui::InputFloat3("New color", customColor.rgb);
+				if (ImGui::Button("Add")) vDitheringPalette.push_back(customColor);
+				if (ImGui::Button("Remove")) vDitheringPalette.erase(vDitheringPalette.begin() + customCursor);
+
+				auto get_item = [](void* data, int idx, const char** output) -> bool
+				{
+					*output = ((def::img::Pixelf*)data)[idx].str().c_str();
+					return true;
+				};
+
+				ImGui::ListBox("Palette", &customCursor, get_item, vDitheringPalette.data(), (int)vDitheringPalette.size());
+			}
+
+			def::img::Dithering_FloydSteinberg(input, output,
 				[&](const def::img::Pixelf& in, def::img::Pixelf& out) {
 					switch (quantMode)
 					{
@@ -267,7 +330,7 @@ protected:
 						break;
 
 					case DitheringQuantiseMode::CustomPalette:
-						DitheringQuantise_CustomPalette(in, out, DITH_PALETTE);
+						DitheringQuantise_CustomPalette(in, out, vDitheringPalette);
 						break;
 					}
 				});
@@ -276,8 +339,10 @@ protected:
 
 		case Filter::Dithering_Ordered:
 		{
-			if (GetKey(def::Key::Z).pressed) matrixMode = DitheringMatrixSizeMode::Two;
-			if (GetKey(def::Key::X).pressed) matrixMode = DitheringMatrixSizeMode::Four;
+			ImGui::Text("Matrix Type");
+
+			if (press_radio("2x2", dithOrdRadio, 2, 0)) matrixMode = DitheringMatrixSizeMode::Two;
+			if (press_radio("4x4", dithOrdRadio, 2, 1)) matrixMode = DitheringMatrixSizeMode::Four;
 
 			switch (matrixMode)
 			{
@@ -294,21 +359,27 @@ protected:
 
 		case Filter::Ripple:
 		{
-			if (GetKey(def::Key::Z).held) rippleCenterX -= 0.1f * dt;
-			if (GetKey(def::Key::X).held) rippleCenterX += 0.1f * dt;
-			if (GetKey(def::Key::C).held) rippleCenterY -= 0.1f * dt;
-			if (GetKey(def::Key::V).held) rippleCenterY += 0.1f * dt;
+			ImGui::SliderFloat2("Center", rippleCenter, 0.0f, 1.0f);
+			def::img::Ripple(input, output, accTime, rippleCenter[0], rippleCenter[1]);
+		}
+		break;
 
-			Ripple(input, output, accTime, rippleCenterX, rippleCenterY);
+		case Filter::CRT:
+		{
+			ImGui::SliderFloat3("Gun", crtGun.rgb, -10.0f, 10.0f);
+			ImGui::SliderFloat3("Skew", crtSkew.rgb, -10.0f, 10.0f);
+			ImGui::SliderFloat3("Align", crtAlign.rgb, -10.0f, 10.0f);
+
+			def::img::CRT(input, output, crtGun, crtSkew, crtAlign);
 		}
 		break;
 
 		}
 
+		ImGui::EndFrame();
+
 		accTime += dt;
 		if (accTime > 2.0f * 3.14159f) accTime = 0.0f;
-
-		Clear(def::DARK_BLUE);
 
 		DrawFrame(input, 10, 10);
 		DrawFrame(output, 470, 10);
@@ -318,39 +389,24 @@ protected:
 		switch (filter)
 		{
 		case Filter::Threshold:
-		{
 			DrawString(50, y, "Filter: Threshold");
-			DrawString(50, y += 16, "Change threshold value with Z and X keys");
-			DrawString(50, y += 16, "Current value = " + std::to_string(threshold));
-		}
-		break;
+			break;
 
 		case Filter::Motion:
 			DrawString(50, y, "Filter: Motion");
 			break;
 
 		case Filter::Convolution:
-		{
 			DrawString(50, y, "Filter: Convolution");
-			DrawString(50, y += 16, "Change convolution filter with Z, X, C, V keys");
-		}
-		break;
+			break;
 
 		case Filter::LowPass:
-		{
 			DrawString(50, y, "Filter: LowPass");
-			DrawString(50, y += 16, "Change lowpass value with Z and X keys");
-			DrawString(50, y += 16, "Current value = " + std::to_string(lowPass));
-		}
-		break;
+			break;
 
 		case Filter::Adaptive:
-		{
 			DrawString(50, y, "Filter: Adaptive Threshold");
-			DrawString(50, y += 16, "Change adaptive threshold value with Z and X keys");
-			DrawString(50, y += 16, "Current value = " + std::to_string(adaptive));
-		}
-		break;
+			break;
 
 		case Filter::Sobel:
 			DrawString(50, y, "Filter: Sobel");
@@ -361,26 +417,19 @@ protected:
 			break;
 
 		case Filter::Dithering_FloydSteinberg:
-		{
 			DrawString(50, y, "Filter: Floyd-Steinberg Dithering");
-			DrawString(50, y += 16, "Change bits per channel count with Z and X keys");
-			DrawString(50, y += 16, "Change modes with C and V keys");
-			DrawString(50, y += 16, "Current value = " + std::to_string(bitsPerChannel));
-		}
-		break;
+			break;
 
 		case Filter::Dithering_Ordered:
-		{
 			DrawString(50, y, "Filter: Ordered Dithering");
-			DrawString(50, y += 16, "Change mode with Z, X keys");
-		}
-		break;
+			break;
 
 		case Filter::Ripple:
 			DrawString(50, y, "Filter: Ripple");
-			DrawString(50, y += 16, "Change ripple center with Z, X, C, V keys");
-			DrawString(50, y += 16, "Current x value = " + std::to_string(rippleCenterX));
-			DrawString(50, y += 16, "Current y value = " + std::to_string(rippleCenterY));
+			break;
+
+		case Filter::CRT:
+			DrawString(50, y, "Filter: CRT");
 			break;
 
 		}
@@ -397,6 +446,15 @@ protected:
 		DrawString(500, y += 16, "8) Floyd-Steinberg Dithering");
 		DrawString(500, y += 16, "9) Ordered Dithering");
 		DrawString(500, y += 16, "0) Ripple");
+		DrawString(500, y += 16, "-) CRT");
+
+		return true;
+	}
+
+	bool OnAfterDraw() override
+	{
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		return true;
 	}
